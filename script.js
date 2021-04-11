b445b277a34849d981032f40d1ae4183846ed721
nkonovalov1990 <n.konovalov1990@gmail.com>
2021-04-11 21:35:37 +0500

    1

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function HTMLToCanvas()
         document.body.appendChild(canvas)
     });
 }
+
+document.getElementById('getval').addEventListener('change', readURL, true);
+function readURL(){
+   var file = document.getElementById("getval").files[0];
+   var reader = new FileReader();
+   reader.onloadend = function(){
+      document.getElementById('circle').style.backgroundImage = "url(" + reader.result + ")";        
+   }
+   if(file){
+      reader.readAsDataURL(file);
+    }else{
+    }
+}
