f076f896628744ed149dd16ae7433ff2f123ffa0
nkonovalov1990 <n.konovalov1990@gmail.com>
2021-05-18 15:29:59 +0500

    1

--- script.js
+++ script.js
@@ -1,84 +1,70 @@
-$("#title").keyup(function(){
-        var str = $("#title").val();
-        $(".title").text(str);
-    });
+$(document).ready(function() {
+    $("input[name='title']").val("Название вакансии");
+    $("textarea[name='description']").val("Описание вакансии");
+    $("textarea[name='tags']").val("фулл тайм, удалёнка, офис");
+});
 
-$("#description").keyup(function(){
-        var str = $("#description").val();
-        if ( str != null){
-            var str = $("#description").val();
-            $(".description").css('display', 'block')
-            $(".description").text(str);
-        } else {
-            $(".description").css('display', 'none')
-        }
-    });
+$(".toggleable").keyup(function(){
+    var value = $(this).val();
+    var recipient = $(this).attr("name");
+    if ( $(this).val() != ""){
+        $("." + recipient).show();
+        $("." + recipient).text(value);
+    } 
+    else {
+        $("." + recipient).hide();
+    }
+});
 
+$("#formTags").keyup(function(){
+    document.getElementById("bannerTags").innerHTML = "";
+    var value = $(this).val();
+    var tags = value.split(',');
+    for(var i = 0; i < tags.length; i++)
+    {
+        document.getElementById("bannerTags").innerHTML += '<div class="tag_bottom">' + tags[i] + '</div>';
+    }
+});
 
 $("div.color_square").click(function(){
-    var BannerColor = $( this ).css( "background-color" );
-    if ( BannerColor != '#FFFFFF') {
+    $(".color_selection > div")
+            .removeClass("selected")
+            .removeClass("selected_white")
+            .css('border', '')
+            .css('box-shadow', '');
+    var BannerColor = $(this).css("background-color");
+    if ( BannerColor == 'rgb(255, 255, 255)') {
+        $(this).addClass("selected_white");
+        $(".banner")
+            .css('background-color', '#FFFFFF')
+            .css('color', '#000000');
         $(".logo")
-            .css('background-image', 'url(images/logo.png)');
+            .css('background-image', 'url(images/logo_black.png)');
+        $(".tag_vacancy")
+            .css('background-color', '')
+            .css('border-color', '#000000')
+            .css('color', '#000000');
+        $(".tag_bottom")
+            .css('background-color', '#000000')
+            .css('color', '#FFFFFF')
+            .css('border-color', '');
+    } else {
+        $(this).addClass("selected");
         $(".banner")
             .css('background-color', BannerColor)
             .css('color', '#FFFFFF');
+        $(".logo")
+            .css('background-image', 'url(images/logo.png)');
         $(".tag_vacancy")
             .css('background-color', '')
             .css('border-color', '#FFFFFF')
             .css('color', '#FFFFFF');
-        $(".tag_fulltime, .tag_remote, .tag_office")
+        $(".tag_bottom")
             .css('background-color', '#FFFFFF')
             .css('color', '#000000')
-            .css('border-color', '#000000');
-    } else {
-        $(".logo")
-            .css('background-image', 'url(images/logo_black.png)');
-        $(".banner")
-            .css('background-color', BannerColor)
-            .css('color', '#000000');
-        $(".tag_vacancy")
-            .css('background-color', '#FFFFFF')
-            .css('border-color', '#000000')
-            .css('color', '#000000');
-        $(".tag_fulltime, .tag_remote, .tag_office")
-            .css('background-color', '#000000')
-            .css('color', '#FFFFFF')
             .css('border-color', '#FFFFFF');
     }
 });
-function changeColor1(){
-    var BannerColor = $('input[name="BannerColor"]:checked').val();
-    if ( BannerColor != '#FFFFFF') {
-        $(".logo")
-            .css('background-image', 'url(images/logo.png)');
-        $(".banner")
-            .css('background-color', BannerColor)
-            .css('color', '#FFFFFF');
-        $(".tag_vacancy")
-            .css('background-color', '')
-            .css('border-color', '#FFFFFF')
-            .css('color', '#FFFFFF');
-        $(".tag_fulltime, .tag_remote, .tag_office")
-            .css('background-color', '#FFFFFF')
-            .css('color', '#000000')
-            .css('border-color', '#000000');
-      } else {
-        $(".logo")
-            .css('background-image', 'url(images/logo_black.png)');
-        $(".banner")
-            .css('background-color', BannerColor)
-            .css('color', '#000000');
-        $(".tag_vacancy")
-            .css('background-color', '#FFFFFF')
-            .css('border-color', '#000000')
-            .css('color', '#000000');
-        $(".tag_fulltime, .tag_remote, .tag_office")
-            .css('background-color', '#000000')
-            .css('color', '#FFFFFF')
-            .css('border-color', '#FFFFFF');
-      }
-}
 
 document.getElementById('getval').addEventListener('change', readURL, true);
 function readURL(){
@@ -93,7 +79,6 @@ function readURL(){
     }
 }
 
-
 function backgroundSizeChange() {
     var BGsize = document.getElementById("BackgroundSizeID").value;
     $(".circle").css('background-size', BGsize + "%");
@@ -101,15 +86,17 @@ function backgroundSizeChange() {
 
 function HTMLToCanvas()
 {
+    window.scrollTo(0, 0);
+
     document.querySelector('canvas').outerHTML = "";
 
     html2canvas(document.querySelector('#banner')).then(canvas => {
         document.querySelector('.result').appendChild(canvas)  
     });;
 
-    // setTimeout(function() {
-    //     document.querySelector('canvas').toBlob(function(blob) {
-    //             saveAs(blob, "netology_banner.png");
-    //     });
-    // }, 100);
+    setTimeout(function() {
+        document.querySelector('canvas').toBlob(function(blob) {
+                saveAs(blob, "netology_banner.png");
+        });
+    }, 100);
 }
